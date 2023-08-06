eaa5dc9f6be98e7c9507fe428820258cefe02f15
DDuyn <duyndavid@gmail.com>
2023-08-06 02:21:45 +0200

    feat: generate game report
    Signed-off-by: DDuyn <duyndavid@gmail.com>

--- src/core/simulator/index.ts
+++ src/core/simulator/index.ts
@@ -7,10 +7,15 @@ enum TeamType {
   LOCAL = "LOCAL",
   AWAY = "AWAY",
 }
+interface PlayerStats {
+  points: number;
+  // Otros datos estadísticos aquí...
+}
 
 interface GameState {
   timeRemainingSeconds: number;
-  teamScores: { [key in TeamType]: number };
+  teamScores: { [key: string]: number };
+  playerStats: { [playerId: number]: PlayerStats };
   ballPossession: {
     team: TeamType;
     player: Player;
@@ -36,10 +41,11 @@ export const basketballSimulation = (localTeam: Team, awayTeam: Team) => {
     startingTeam === TeamType.LOCAL ? localPlayers : awayPlayers;
 
   const gameState: GameState = {
-    timeRemainingSeconds: 400,
+    timeRemainingSeconds: 720,
+    playerStats: {},
     teamScores: {
-      LOCAL: 0,
-      AWAY: 0,
+      [localTeam.name]: 0,
+      [awayTeam.name]: 0,
     },
     ballPossession: {
       team: startingTeam,
@@ -52,10 +58,10 @@ export const basketballSimulation = (localTeam: Team, awayTeam: Team) => {
     const timeForAction = Math.floor(Math.random() * 24) + 1;
     gameState.timeRemainingSeconds -= timeForAction;
 
-    const attackingTeamPlayers =
+    /*const attackingTeamPlayers =
       gameState.ballPossession.team === TeamType.LOCAL
         ? localPlayers
-        : awayPlayers;
+        : awayPlayers;*/
     const defendingTeamPlayers =
       gameState.ballPossession.team === TeamType.LOCAL
         ? awayPlayers
@@ -73,12 +79,26 @@ export const basketballSimulation = (localTeam: Team, awayTeam: Team) => {
     const attackType = "lanza de 2";
 
     if (attackResult === SimulationResult.SUCCESS) {
-      gameState.teamScores[gameState.ballPossession.team] += 2;
+      const points = 2; // Ajusta esto según sea necesario
+
+      gameState.teamScores[
+        gameState.ballPossession.team === TeamType.LOCAL
+          ? localTeam.name
+          : awayTeam.name
+      ] += points;
+
+      const playerId = gameState.ballPossession.player.id;
+      if (!gameState.playerStats[playerId]) {
+        gameState.playerStats[playerId] = { points: 0 };
+      }
+
+      gameState.playerStats[playerId].points += points;
+
       gameState.ballPossession.team =
         gameState.ballPossession.team === TeamType.LOCAL
           ? TeamType.AWAY
           : TeamType.LOCAL;
-      gameState.ballPossession.player = getRandomPlayer(attackingTeamPlayers);
+      gameState.ballPossession.player = getRandomPlayer(defendingTeamPlayers);
       gameState.events.push(
         `Jugador ${attackerName} lleva el balón y ${attackType}, anota.`
       );
@@ -96,7 +116,33 @@ export const basketballSimulation = (localTeam: Team, awayTeam: Team) => {
     if (gameState.timeRemainingSeconds <= 0) break;
   }
 
+  generateReport(gameState, localTeam, awayTeam);
   const report = gameState.events.join("\n");
   console.log(report);
-  console.table(gameState.teamScores);
+};
+
+const generateReport = (
+  gameState: GameState,
+  localTeam: Team,
+  awayTeam: Team
+) => {
+  console.log(
+    `Resultado final: ${localTeam.name} ${
+      gameState.teamScores[localTeam.name]
+    } - ${awayTeam.name} ${gameState.teamScores[awayTeam.name]}`
+  );
+
+  for (const team of [localTeam, awayTeam]) {
+    console.log(`Estadísticas del equipo ${team.name}:`);
+    // Aquí podrías imprimir estadísticas de equipo adicionales
+
+    console.log(`Estadísticas de los jugadores:`);
+    for (const player of team.players) {
+      const stats = gameState.playerStats[player.id];
+      console.log(
+        `Nombre del jugador: ${player.personalInfo.name} ${player.personalInfo.surname} - ${stats.points}`
+      );
+      // Aquí podrías imprimir estadísticas del jugador adicionales
+    }
+  }
 };
